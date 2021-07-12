e3e789c883bba8d97af723cc9d0b1b35e30a40a8
Christian Duarte <cristiands1109@gmail.com>
2021-07-12 19:16:07 -0400

    assistapp-backend prueba obtener geo

--- routes/geo.routes.js
+++ routes/geo.routes.js
@@ -10,11 +10,11 @@ router.get('/', function (req = request, resp = response) {
    
    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
 
-   if (req.socket.remoteAddress ) {
-      return resp.status(404).json({
-         msg: 'No es posible obtener la ubicacion, contacte con el administrador'
-      })
-   }
+   // if (req.socket.remoteAddress ) {
+   //    return resp.status(404).json({
+   //       msg: 'No es posible obtener la ubicacion, contacte con el administrador'
+   //    })
+   // }
 
    const geo = geoIP.lookup(ip)
 
