62288ca7a88da9e0240b0d4a4f7c003a1e377037
Christian Duarte <cristiands1109@gmail.com>
2021-07-12 18:41:42 -0400

    assistapp-backend prueba obtener geo

--- routes/geo.routes.js
+++ routes/geo.routes.js
@@ -8,15 +8,20 @@ const router = Router();
 
 router.get('/', function (req = request, resp = response) {
   
+   const ip = req.socket.remoteAddress;
 
-   const ip2 = "207.97.227.239"
-   const ge2 = geoIP.lookup(ip2)
+   // const ip2 = "207.97.227.239"
+   // const ge2 = geoIP.lookup(ip2)
 
-   const [la, lon] = ge2.ll
-   console.log(la);
-   console.log(lon);
+   // const [la, lon] = ge2.ll
+   // console.log(la);
+   // console.log(lon);
 
-   console.log('ip2' , ge2);
+   // console.log('ip2' , ge2);
+
+   resp.status(200).json({
+      ip
+   })
 
    
 });
