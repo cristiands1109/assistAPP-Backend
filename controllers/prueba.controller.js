e19d59630eb53191e7323e744b05395ee5a09a32
Christian Duarte <cristiands1109@gmail.com>
2021-07-15 19:28:33 -0400

    assistapp-backend - prrueba corre

--- controllers/prueba.controller.js
+++ controllers/prueba.controller.js
@@ -11,8 +11,8 @@ const { getResultGeo } = require("../helpers/getGeo.helper");
 const prueba = async (req = request, resp = response) => {
     
     const accesskey = '4c2594c643861f24b775bb0d5ca4086e';
-    // const ip = req.headers['x-forwarded-for'];
-    const ip = '181.123.0.29';
+    const ip = req.headers['x-forwarded-for'];
+    // const ip = '181.123.0.29';
     // const ip = '170.51.53.148';
 
     if(!ip) {
