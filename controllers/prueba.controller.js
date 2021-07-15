1d57de04d4c51b3231ad90ce5c29b92f1b151fcc
Christian Duarte <cristiands1109@gmail.com>
2021-07-14 21:34:03 -0400

    assistapp-backend - prueba

--- controllers/prueba.controller.js
+++ controllers/prueba.controller.js
@@ -1,24 +1,32 @@
 const { response, request } = require("express");
 // const { getDATA, getResultado } = require('../helpers/getGeo.helper');
-const ipstack = require('ipstack');
+// const ipstack = require('ipstack');
 const { getResultGeo } = require("../helpers/getGeo.helper");
 
 
-const point = 'http://api.ipstack.com';
 // const ipprueba = req.headers['x-forwarded-for'] || '181.123.0.29';
-const ipprueba = req.headers['x-forwarded-for'];
-// const ipprueba = '181.123.0.29';
-const accesskey = '4c2594c643861f24b775bb0d5ca4086e';
-const url = `${point}/${ipprueba}?access_key=${accesskey}`;
 
 
 
 const prueba = async (req = request, resp = response) => {
-  
-
-    const data = await getResultGeo(ipprueba, accesskey);
+    
+    const accesskey = '4c2594c643861f24b775bb0d5ca4086e';
+    const ip = req.headers['x-forwarded-for'];
+    // const ip = '181.123.0.29';
+    if(!ip) {
+       return resp.json({
+           msg: 'No se puede obtener la ubicacion, favor contactar con el ADMINISTRADOR'
+       })
+    }
+    const data = await getResultGeo(ip, accesskey);
+    
     console.log('hola desde nose', data);
-   
+    const [lat, lng] = [data.latitude, data.longitude]
+    resp.json({
+        lat,
+        lng,
+        data
+    })
     
 }
 
